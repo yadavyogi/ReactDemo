8eb89d5ca7c4d832ff40bf37bcb5d5a46efb9605
yadavyogi <yk243151@gmail.com>
2023-06-13 23:52:45 +0530

    hii

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+import {ButtonGroup, Button} from '@material-ui/core'
+import './App.css';
+function App() {
+  return (
+    <div classname="App">
+      <h1>React Material UI | ButtonGroup</h1>
+      <ButtonGroup
+      orientation="vertical"
+      varient="outlined" color="primary">
+
+        <Button>one</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+        <Button>Four</Button>
+      </ButtonGroup>
+    </div>
+  )
+}
+
+export default App;
+
